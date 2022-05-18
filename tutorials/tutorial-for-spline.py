26865ad40fc0057d91608bad9bdcd30f15c1ba61
mengj <813935532@qq.com>
2022-05-18 17:59:59 +0800

    feat: add spline

---
+++ tutorials/tutorial-for-spline.py
@@ -0,0 +1,13 @@
+import ezdxf
+
+doc = ezdxf.new('R2000')
+
+fit_points = [(0, 0, 0), (750, 500, 0), (1750, 500, 0), (2250, 1250, 0)]
+msp = doc.modelspace()
+spline = msp.add_spline(fit_points)
+spline.fit_points.append((2250, 2500, 0))
+msp.add_spline_control_frame(fit_points, method='uniform', dxfattribs={'color': 1})
+msp.add_spline_control_frame(fit_points, method='chord', dxfattribs={'color': 3})
+msp.add_spline_control_frame(fit_points, method='centripetal', dxfattribs={'color': 5})
+
+doc.saveas('../output/spline.dxf')
