9c738b400ed4c0516cc89c13ccf88d685ce1cf34
mengj <813935532@qq.com>
2022-05-18 15:35:00 +0800

    feat: add line

---
+++ tutorials/tutorial-for-creating-simple-dxf-drawings.py
@@ -0,0 +1,10 @@
+import ezdxf
+
+# Create a new DXF R2010 drawing, official DXF version name: "AC1024"
+doc = ezdxf.new('R2010')
+
+# Add new entities to the modelspace:
+msp = doc.modelspace()
+# Add a LINE entity
+msp.add_line((0, 0), (10, 0))
+doc.saveas("../output/line.dxf")
