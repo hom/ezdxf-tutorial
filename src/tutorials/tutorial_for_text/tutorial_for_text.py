6a407c843f464408bcbb489fa80c545fb460352c
mengj <813935532@qq.com>
2022-05-29 00:19:30 +0800

    feat: add text

--- src/tutorials/tutorial_for_text/tutorial_for_text.py
+++ src/tutorials/tutorial_for_text/tutorial_for_text.py
@@ -1,8 +1,9 @@
 import ezdxf
 
-doc = ezdxf.new('R12', setup=True)
+doc = ezdxf.new("R2018", setup=True, units=ezdxf.units.MM)
 msp = doc.modelspace()
 
+'''
 msp.add_text('A Simple Text').set_pos((2, 3), align='MIDDLE_RIGHT')
 
 msp.add_text('Text Style Example: Liberation Serif',
@@ -10,5 +11,11 @@ msp.add_text('Text Style Example: Liberation Serif',
                  'style': 'LiberationSerif',
                  'height': 0.35
              }).set_pos((2, 6), align='LEFT')
+'''
 
+msp.add_text("AC", dxfattribs={}).set_pos((10, 10))
+msp.add_text("AC", dxfattribs={
+    "height": 4,
+    "bold": True,
+}).set_pos((0, 0))
 doc.saveas('output/simple_text.dxf')
