eaaa3d1465134feb38bd38c2ff4b23b03ae5e0b6
mengj <813935532@qq.com>
2022-05-28 22:16:05 +0800

    feat: add ac symbol

--- src/tutorials/tutorial_-for_lwpolyline/tutorial_-for_lwpolyline.py
+++ src/tutorials/tutorial_-for_lwpolyline/tutorial_-for_lwpolyline.py
@@ -25,7 +25,6 @@ msp.add_lwpolyline(points)
 doc.saveas("output/lwpolyline4.dxf")
 '''
 
-
 # lwpolyline 5
 '''
 doc = ezdxf.new("R2010")
@@ -38,12 +37,34 @@ doc.saveas('lwpolyline5.dxf')
 '''
 
 # lwpolyline 6
-
 '''
 doc = ezdxf.new('R2010')
 msp = doc.modelspace()
 msp.add_lwpolyline([(0, 0, 0), (10, 0, 1), (20, 0, 0)], format='xyb')
 msp.add_lwpolyline([(0, 10, 0), (10, 10, .5), (20, 10, 0)], format='xyb')
+msp.add_lwpolyline([(0, 20, 0.5), (10, 20, 0)], format="xyb")
+msp.add_lwpolyline([(0, 30, 2), (10, 30, 0)], format="xyb")
+msp.add_lwpolyline([(0, 40, 0.5), (10, 40, 0), (20, 40, -0.5), (30, 40, 0)],
+                   format="xyb")
+msp.add_lwpolyline([(0, 50, 0.5), (10, 50, -0.5), (20, 50, 0)],
+                   format="xyb")
 
 doc.saveas("output/lwpolyline6.dxf")
 '''
+
+# lwpolyline 7
+doc = ezdxf.new('R2018', setup=True, units=ezdxf.units.MM)
+msp = doc.modelspace()
+msp.add_lwpolyline([(0.3111, 0, -0.7627), (-3.7404, 0, 0.7627),
+                    (-7.7919, 0, 0)],
+                   format='xyb')
+msp.add_linear_dim(base=[0, 12], p1=(0.3111, 0), p2=(0, 0), dimstyle="EZDXF")
+
+
+msp.add_lwpolyline([(0, 10, -0.7627), (-4.0515, 10, 0.7627),
+                    (-8.1030, 10, 0)],
+                   format='xyb')
+
+msp.add_line((0, 0), (0, 20))
+
+doc.saveas("output/lwpolyline7.dxf")
