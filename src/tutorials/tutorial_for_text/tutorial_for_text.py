abdbf59d0462ab7e44898db0c0fe6934d92f8f1a
mengj <813935532@qq.com>
2022-05-29 11:09:01 +0800

    feat: text tutorial

--- src/tutorials/tutorial_for_text/tutorial_for_text.py
+++ src/tutorials/tutorial_for_text/tutorial_for_text.py
@@ -13,9 +13,14 @@ msp.add_text('Text Style Example: Liberation Serif',
              }).set_pos((2, 6), align='LEFT')
 '''
 
+doc.styles.new('SimSun', dxfattribs={'font': 'simsun.ttf'})
 msp.add_text("AC", dxfattribs={}).set_pos((10, 10))
 msp.add_text("AC", dxfattribs={
     "height": 4,
-    "bold": True,
+    "style": "SimSun",
 }).set_pos((0, 0))
+
+msp.add_lwpolyline([(0, 0), (0, 4), (4, 4), (4, 0)], close=True, dxfattribs={
+    "color": 4,
+})
 doc.saveas('output/simple_text.dxf')
